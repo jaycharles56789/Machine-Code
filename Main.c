0f6b11babf8018b1c6e088f670b127c2b6c81324
QuiSensei <phillipnathanaelquieta@gmail.com>
2025-11-10 23:54:16 +0800

    add main prevention

--- Main.c
+++ Main.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "mips64.h"
 
 int main(void) {
-    char c_based_variable[128];
-    printf("");
+    char c_based_variable[256];
+
     printf("Enter a simple C-based variable assignment\n(e.g., int a = 5; || int b; || int c = a + b; || a + b + c):\n");
 
-    fgets(c_based_variable, sizeof(c_based_variable), stdin);
-    c_based_variable[strcspn(c_based_variable, "\n")] = '\0';
+    while(1) {
 
-    transform_to_assembly(c_based_variable);
+        fgets(c_based_variable, sizeof(c_based_variable), stdin);
+        c_based_variable[strcspn(c_based_variable, "\n")] = '\0';
+
+        // Check if the input starts with "int "
+        if(strncmp(c_based_variable, "int ", 4) == 0) {
+            // skipping "int " and space
+            char *var = c_based_variable + 4; 
+            while (*var == ' ') var++; 
+
+            if(strcmp(var, "int") == 0) {
+                printf("Invalid: Data type \n");
+                continue;
+            }
+
+            // Reject if empty or only semicolon
+            if (*var == ';' || *var == '\0') {
+                printf("Invalid: missing variable name\n");
+                continue;
+            }
+
+            // Reject if first char of var is not a letter or underscore
+            if (!isalpha(*var)) {
+                printf("Invalid: variable must start with a letter\n");
+                continue;
+            }
+        } else{
+            printf("Invalid: missing data type 'int'\n");
+            continue;
+        }
+
+        break;
+    }
 
     
     return 0;
--- mips64.h
+++ mips64.h
@@ -1,7 +1,13 @@
 #ifndef _MIPS64_H_
 #define _MIPS64_H_
 
+// Function prototypes for Registers.c
 int tranform_to_assembly(const char *input_line);
 int get_register_code(const char *name, char *out_code);
+
+// Function prototypes for I-type.c
+
+// Function prototypes for R-type.c
+
 
 #endif
