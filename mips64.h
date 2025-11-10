5b0bc2306492a6e6b779a1687a251144891693f2
jaycharles56789 <charlesjayson56789@gmail.com>
2025-11-10 23:42:02 +0800

    check

--- mips64.h
+++ mips64.h
@@ -1,7 +1,7 @@
 #ifndef _MIPS64_H_
 #define _MIPS64_H_
 
-int tranform_to_assembly();
-int get_register_code();
+int tranform_to_assembly(const char *input_line);
+int get_register_code(const char *name, char *out_code);
 
 #endif
