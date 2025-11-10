fb6f48f115f5c3d89016ddfc900a5b7a49ca7324
QuiSensei <phillipnathanaelquieta@gmail.com>
2025-11-10 22:40:17 +0800

    Working

--- Main.c
+++ Main.c
@@ -2,19 +2,14 @@
 #include <string.h>
 #include "mips64.h"
 
-void whitespace_tream(char *str);
-
 int main(void) {
-    char string[128];
-    return 0;
-}
+    char c_based_variable[128];
+    printf("");
+    printf("Enter a simple C-based variable assignment\n(e.g., int a = 5; || int b; || int c = a + b; || a + b + c):\n");
 
-void whitespace_tream(char *str) {
-    int j = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n') {
-            str[j++] = str[i];
-        }
-    }
-    str[j] = '\0';
+    fgets(c_based_variable, sizeof(c_based_variable), stdin);
+    c_based_variable[strcspn(c_based_variable, "\n")] = '\0';
+
+    
+    return 0;
 }
