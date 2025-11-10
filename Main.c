80e72893569a36fa301077c27cfcf360e8fa0136
QuiSensei <phillipnathanaelquieta@gmail.com>
2025-11-10 20:53:23 +0800

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include "mips64.h"
 
+void whitespace_tream(char *str);
+
 int main(void) {
-    
+    char string[128];
     return 0;
+}
+
+void whitespace_tream(char *str) {
+    int j = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n') {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
 }
