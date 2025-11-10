5f554b6baa970ebf3da5d30e042cae7bddd89e6b
jaycharles56789 <charlesjayson56789@gmail.com>
2025-11-11 00:14:25 +0800

    s

--- mips64.h
+++ mips64.h
@@ -1,13 +1,31 @@
 #ifndef _MIPS64_H_
 #define _MIPS64_H_
 
-// Function prototypes for Registers.c
-int tranform_to_assembly(const char *input_line);
-int get_register_code(const char *name, char *out_code);
+#include <stddef.h> //for size_t
 
-// Function prototypes for I-type.c
+#define REG_CODE_STR_LEN 6
+#define OPCODE_STR_LEN 7
 
-// Function prototypes for R-type.c
-
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+int get_register_code(const char *name, char out_code[REG_CODE_STR_LEN]);
+
+int get_register_name(const char code[REG_CODE_STR_LEN - 1],
+                      char *out_name, size_t out_name_sz);
+
+int get_i_opcode(const char *mnemonic, char out_code[OPCODE_STR_LEN]);
+int get_j_opcode(const char *mnemonic, char out_code[OPCODE_STR_LEN]);
+
+int get_r_opcode(const char *mnemonic, char out_code[OPCODE_STR_LEN]);
+
+int transform_to_assembly(const char *input_line,
+                          char *out_buf, size_t out_buf_sz);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _MIPS64_H_
+
